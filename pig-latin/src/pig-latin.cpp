90e1afdc7b37a1fe673c0d7345c03005a89e407b
John Freeman <jfreeman08@gmail.com>
2024-11-04 17:07:25 -0600

    Import fmt
    ```
    cupcake add fmt
    ```

--- pig-latin/src/pig-latin.cpp
+++ pig-latin/src/pig-latin.cpp
@@ -1,13 +1,15 @@
 #include <cstdio>
 #include <cstdlib>
 
+#include <fmt/base.h>
+
 #include <pig-latin/pig-latin.hpp>
 
 int main(int argc, const char** argv) {
     if (argc < 2) {
-        std::printf("usage: %s <word>\n", argv[0]);
+        fmt::println("usage: {} <word>", argv[0]);
         return EXIT_FAILURE;
     }
-    std::printf("%s\n", pig_latin::word(argv[1]).c_str());
+    fmt::println("{}", pig_latin::word(argv[1]));
     return 0;
 }
